0179d6017c3af2ecd16fb6e213947541523afd4e
Ashraf Ali <ashrafali379@gmail.com>
2020-07-12 23:21:41 +0100

    Added prediction pipeline

--- src/evaluate.py
+++ src/evaluate.py
@@ -16,6 +16,7 @@ def generate_results(model_name):
     predictions = model.predict(
         test_gen, steps=test_gen.n / test_gen.batch_size)
     header = open('../predictions/prediction_format.csv', 'r').readlines()[0]
+    
     with open('../predictions/prediction_1.csv', 'w') as outfile:
         outfile.write(header)
         for i in range(len(test_gen.filenames)):
